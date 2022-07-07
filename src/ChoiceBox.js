780d67270be365c204d93ed5b33edaec4e66ad7a
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-07 14:17:10 -0500

    adds choice box to page

--- src/ChoiceBox.js
+++ src/ChoiceBox.js
@@ -1,7 +1,24 @@
 const ChoiceBox = () => {
   return (
     <div className="choice-container">
-      hi again
+      <div className="title">
+        Anime title
+      </div>
+      <img
+        src="https://static.wikia.nocookie.net/yakusokunoneverland/images/c/cb/Anime_date_announcement.png/revision/latest?cb=20190112054937"
+        width="360px"
+        height="200px"
+        alt="Anime Cover Art"
+        className="cover"
+      />
+      <div className="anime-summary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed excepturi ipsum nesciunt aut reiciendis. Ipsam est accusamus recusandae
+      </div>
+      <div className="choice-gnere-list">
+        <span className="choisce-genre">genre1</span>
+        <span className="choisce-genre">genre2</span>
+        <span className="choisce-genre">genre3</span>
+      </div>
     </div>
   );
 }
