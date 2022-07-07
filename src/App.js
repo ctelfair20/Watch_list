c9c6641d05723c2dcb0e714bb9baf4a1a8013f9f
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-07 12:10:00 -0500

    header added to page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
+// echo "# Watch_list" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/ctelfair20/Watch_list.git
+// git push -u origin main
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="header-content">
+        <span>Anime WatchList</span>
+      </div>
+      <div className="header-content">
+        <button>
+          Add +
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
