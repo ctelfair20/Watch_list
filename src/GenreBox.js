12f34a3a395a1f2e65dca0cc296aa55347e5024c
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-29 17:20:44 -0500

    alphabetized genre list

--- src/GenreBox.js
+++ src/GenreBox.js
@@ -10,21 +10,21 @@ const GenreBox = () => {
           <input type="radio" /> Action
           <input type="radio" /> Comedy
           <input type="radio" /> Drama
-        </div>
-        <div className="genre-column">
           <input type="radio" /> Fantasy
-          <input type="radio" /> Slice of Life
-          <input type="radio" /> Sports
-          <input type="radio" /> Supernatural
-          <input type="radio" /> Magic
-          <input type="radio" /> Mystery
         </div>
         <div className="genre-column">
           <input type="radio" /> Historical
           <input type="radio" /> Horror
+          <input type="radio" /> Magic
+          <input type="radio" /> Mystery
           <input type="radio" /> Psychological
-          <input type="radio" /> SciFi
+        </div>
+        <div className="genre-column">
           <input type="radio" /> Romance
+          <input type="radio" /> SciFi
+          <input type="radio" /> Slice of Life
+          <input type="radio" /> Sports
+          <input type="radio" /> Supernatural
         </div>
       </div>
       <button>
