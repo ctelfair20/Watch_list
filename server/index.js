6223bd1ce251fbf9107d17b374fb87b154871464
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-29 16:48:34 -0500

    set up server and handlers functions

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const { getHome } = require('./handlers.js');
 const app = express();
 const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', getHome);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
