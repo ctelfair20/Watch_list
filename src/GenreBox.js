e3762db7c5aa9ff7b5e865cf2d8ad7488320dbe3
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-31 20:53:37 -0500

    gets back anime list from api

--- src/GenreBox.js
+++ src/GenreBox.js
@@ -1,8 +1,35 @@
+import axios from 'axios'
+
 const GenreBox = () => {
 
-  const handleOnClick = () => {
-    console.log('the click works')
+  const handleOnClick = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://anime-db.p.rapidapi.com/anime',
+      params: {
+        page: '1',
+        size: '10',
+        search: 'Fullmetal',
+        genres: 'Fantasy,Drama',
+        sortBy: 'ranking',
+        sortOrder: 'asc'
+      },
+      headers: {
+        'X-RapidAPI-Key': 'e84df027ddmshefa646604ede5f9p193db4jsn2b9b1627633f',
+        'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+      }
+    }
+
+    try {
+      const data = await axios.request(options);
+      console.log(data.data)
+    } catch (err) {
+      console.log(`ERROR: ${err}`)
+    } finally {
+      console.log('this did a thing')
+    }
   }
+
   return (
     <div className="genre-container">
       <div className="title">
