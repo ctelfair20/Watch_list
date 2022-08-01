5814999ab6050b3ce856d4e875b4117ac86bec92
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-08-01 13:21:04 -0500

    cleanup genrebox

--- src/GenreBox.js
+++ src/GenreBox.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import CheckBox from './CheckBox'
-import { getAnime } from './requestHandlers'
+import CheckBox from './CheckBox';
+import { getAnime } from './requestHandlers';
 
 const GenreBox = () => {
   const [checked, setChecked] = useState([]);
@@ -8,11 +8,11 @@ const GenreBox = () => {
   const handleChecked = (e) => {
     let updateCheckedGenres = [...checked];
     if (e.target.checked) {
-      updateCheckedGenres = [...checked, e.target.value]
-      setChecked(updateCheckedGenres)
+      updateCheckedGenres = [...checked, e.target.value];
+      setChecked(updateCheckedGenres);
     } else {
-      updateCheckedGenres.splice(updateCheckedGenres.indexOf(e.target.value), 1)
-      setChecked(updateCheckedGenres)
+      updateCheckedGenres.splice(updateCheckedGenres.indexOf(e.target.value), 1);
+      setChecked(updateCheckedGenres);
     }
   }
 
@@ -22,21 +22,21 @@ const GenreBox = () => {
     "Comedy",
     "Drama",
     "Fantasy",
-  ]
+  ];
   const genreList2 = [
     "Historical",
     "Horror",
     "Magic",
     "Mystery",
     "Psychological"
-  ]
+  ];
   const genreList3 = [
     "Romance",
     "SciFi",
     "Slice of Life",
     "Sports",
     "Supernatural",
-  ]
+  ];
 
   return (
     <div className="genre-container">
@@ -44,27 +44,9 @@ const GenreBox = () => {
         Pick A Genre!
       </div>
       <div className="genre-lists">
-        <div className="genre-column">
-          {genreList1.map((item, index) => (
-            <div key={index} className="genre">
-              <input value={item} type="checkbox" onChange={handleChecked} />{item}
-            </div>
-          ))}
-        </div>
-        <div className="genre-column">
-          {genreList2.map((item, index) => (
-            <div key={index} className="genre">
-              <input value={item} type="checkbox" onChange={handleChecked} />{item}
-            </div>
-          ))}
-        </div>
-        <div className="genre-column">
-          {genreList3.map((item, index) => (
-            <div key={index} className="genre">
-              <input value={item} type="checkbox" onChange={handleChecked} />{item}
-            </div>
-          ))}
-        </div>
+        <CheckBox genreList={genreList1} onCheck={handleChecked} />
+        <CheckBox genreList={genreList2} onCheck={handleChecked} />
+        <CheckBox genreList={genreList3} onCheck={handleChecked} />
       </div>
       <button onClick={getAnime}>
         random Anime!
